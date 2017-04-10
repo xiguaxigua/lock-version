784eaedfbc3891b132df4cc8ad02112d07b74e0d
quietcoder <1642965215@qq.com>
2017-04-10 17:27:43 +0800

    fix: path error

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-var package = require('./package.json')
+var package = require(process.cwd() + '/package.json')
 var fs = require('fs')
 
 var dependencies = [package.dependencies, package.devDependencies]
 dependencies.forEach(dependenceObj => {
-  Object.keys(dependenceObj).forEach(depName => {
+  dependenceObj && Object.keys(dependenceObj).forEach(depName => {
     dependenceObj[depName] = getModulesVersion(depName)
   })
 })
 
-fs.writeFile('./package.json', JSON.stringify(package, null, 2), function (err) {
+fs.writeFile(process.cwd() + '/package.json', JSON.stringify(package, null, 2), function (err) {
   var noticeText = err ? '写入失败: ' + err : '写入成功'
   console.log(noticeText)
 })
 
 function getModulesVersion (moduleName) {
-  var module = JSON.parse(fs.readFileSync('./node_modules/' + moduleName + '/package.json').toString())
+  var module = JSON.parse(fs.readFileSync(process.cwd() + '/node_modules/' + moduleName + '/package.json').toString())
   return module.version
 }
